f8bec13656b299c0b30d9ca6ad9a651ccbd9d2b0
CrazY <panchi.psp@gmail.com>
2019-07-13 02:49:12 +0900

    Chromeで完全に動くようになった。Firefoxは2文目が入力できない。

--- send_pdf_to_mirai.py
+++ send_pdf_to_mirai.py
@@ -7,7 +7,8 @@
 pdfを読み取ってみらい翻訳に投げるスクリプト
 """
 import os
-import re
+import subprocess
+# import re
 import sys
 from io import StringIO
 # from getpass import getpass  # パスワード入力用
@@ -19,27 +20,27 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfpage import PDFPage
+# from pysnooper import snoop
 from selenium import webdriver
-# from selenium.webdriver.common.alert import Alert
+from selenium.common import exceptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-
-# from pysnooper import snoop
-
-
+from tqdm import tqdm
 
 # ブラウザを指定(Firefox,Chrome,Edge)
-BROWSER_NAME = 'Chrome'
+BROWSER_NAME = 'Firefox'
 # 実行ファイルの絶対パスを取得
 ABS_DIRNAME = os.path.dirname(os.path.abspath(__file__))
 
-# ここからコピペ
+# テストモード（有効にすると標準出力が増える）
+TEST_MODE = False
 
 
 def gettext(pdfname):
     """
     PDFファイルを読み取って文字列を返す関数
+    ほぼコピペ→https://arakan-pgm-ai.hatenablog.com/entry/2018/01/07/080000
     """
     # PDFファイル名が未指定の場合は、空文字列を返して終了
     if pdfname == '':
@@ -48,7 +49,8 @@ def gettext(pdfname):
     # 処理するPDFファイルを開く/開けなければ
     try:
         fp = open(pdfname, 'rb')
-    except:
+    except FileNotFoundError as e:
+        print(e)
         return ''
 
     # リソースマネージャインスタンス
@@ -64,7 +66,8 @@ def gettext(pdfname):
     # テキスト抽出インタプリタインスタンス
     interpreter = PDFPageInterpreter(rsrcmgr, device)
     # 対象ページを読み、テキスト抽出する。（maxpages：0は全ページ）
-    for page in PDFPage.get_pages(fp, pagenos=None, maxpages=0, password=None, caching=True, check_extractable=True):
+    pages = PDFPage.get_pages(fp, pagenos=None, maxpages=0, caching=True, check_extractable=True)
+    for page in pages:
         interpreter.process_page(page)
     # 取得したテキストをすべて読みだす
     ret = outfp.getvalue()
@@ -77,6 +80,35 @@ def gettext(pdfname):
     return ret
 
 # ------------ここまでコピペ-------------------------
+
+
+def split_txt(txt):
+    """
+    文章を区切ってリストで返す
+    txt:文章の文字列
+    """
+    # 文を区切る長さ
+    # n = 5
+
+    # テキストをピリオドで区切ってリストにする
+    # ピリオドが消えるから元に戻す
+    # l_1 = list(txt.split('.'))
+    # for i in range(len(l_1)):
+    #     if l_1[i] not in ('\n', '\n\n', '\n\n\x0c'):
+    #         l_1[i] += '.'
+
+    # 5文ごとにまとめる
+    # l_2 = []
+    # m = len(l_1) // n
+    # for i in range(m + 1):
+    #     l_2.append(l_1[i * 5: (i + 1) * 5])
+    #     l_2.append(l_1[(i + 1) * 5:])
+
+    # 5文ごとのリストを返す
+    # return l_2
+
+    # テキストを改行で区切ってリストで返す
+    return list(txt.split('\n'))
 
 
 def select_driver(browser):
@@ -140,85 +172,79 @@ def select_driver(browser):
     sys.exit()
 
 
-def split_txt(txt):
-    """
-    文章を5文ごとに分けてリストで返す
-    txt:文章の文字列
-    """
-    # 文を区切る長さ
-    n = 5
-
-    # テキストをピリオドで区切ってリストにする
-    # ピリオドが消えるから元に戻す
-    l_1 = []
-    for sentence in re.split('.', txt):
-        if sentence != '.':
-            l_1.append(sentence)
-        elif sentence == '':
-            pass
-        else:
-            l_1[-1] += sentence
-
-    # 5文ごとにまとめる
-    l_2 = []
-    n = len(l_1) // n
-    for i in range(n):
-        l_2.append(l_1[i * 5: (i + 1) * 5])
-    l_2.append(l_1[(i + 1) * 5:])
-
-    # 5文ごとのリストを返す
-    return l_2
-
-
+# @snoop()
 def use_miraitranslate(d, l):
     """
     文字列をみらい翻訳に送りつけて、翻訳結果を文字列で返す関数
     d: webdriver, txt: 翻訳したい文章
     """
-    time = 2
-    wait = WebDriverWait(d, 10)
+
     # サイトを開く
     d.get('https://miraitranslate.com/trial/')
+
+    wait = WebDriverWait(d, 10)
     wait.until(EC.presence_of_all_elements_located)
+
+    # 原文言語を日本語に設定
     d.find_element_by_class_name('sourceLanguageDiv').click()
     wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/span/span/span[2]/ul/li[2]')))
     d.find_element_by_xpath('/html/body/span/span/span[2]/ul/li[2]').click()
-    d.find_element_by_id('translateSourceInput').click()
 
-    translated = ''
-    print(l)
-    for en in l:
-        sleep(time)
-        d.find_element_by_id('translateSourceInput').send_keys(en)
-        sleep(time)
+    # 原文入力欄を記憶
+    txt_in = d.find_element_by_id('translateSourceInput')
+
+    # 翻訳作業
+    answer = ''
+    for v in tqdm(l):
+        # 原文を入力
+        txt_in.send_keys(v)
+        # 翻訳ボタンをクリック
+        wait.until(EC.element_to_be_clickable((By.ID, 'translateButtonTextTranslation')))
         d.find_element_by_id('translateButtonTextTranslation').click()
-        sleep(time)
-        ja = d.find_element_by_id('translate-text').getAttribute('value')
-        translated += ja
-    return translated
+        sleep(1)
+        wait.until(EC.element_to_be_clickable((By.ID, 'translate-text')))
+        # 翻訳結果を取得
+        translated = d.find_element_by_id('translate-text').text + '\n'
+        answer += translated
+        # 原文を消去
+        txt_in.clear()
+    return answer
 
 
 def main():
+    """
+    1. PDFファイル名を指定
+    2. PDFファイルからテキストを読み取る
+    3. テキストを改行で区切る
+    4. みらい翻訳に送って翻訳する
+    5. 翻訳結果をTXTファイルに保存する
+    """
     pdfname = ABS_DIRNAME + '/' + input('PDFファイル名を入力してください。\n>>> ')
+    pdfname = ABS_DIRNAME + '/input.pdf'
 
     print('PDFを読み取ります。')
     txt = gettext(pdfname)
-    print('txt----------')
-    print(txt)
-    print('txt----------')
+    if TEST_MODE:
+        print('----------pdf----------')
+        print(txt)
+        print('----------pdf----------\n')
     print('PDFを読み取りました。')
 
     print('文章を分割します。')
     l = split_txt(txt)
-    print('l:')
-    pprint('l')
+    if TEST_MODE:
+        print('l:')
+        pprint(l)
     print('文章を分割しました。')
 
     try:
-        print('みらい翻訳で翻訳します。')
+        print('ブラウザを起動します。')
         d = select_driver(BROWSER_NAME)
+        print('みらい翻訳で翻訳します。')
         translated = use_miraitranslate(d, l)
         print('みらい翻訳が完了しました。')
+        d.close()
+        print('ブラウザを終了しました。')
 
         print('ファイル出力を開始します。')
         path = ABS_DIRNAME + '/mirai_output.txt'
@@ -226,11 +252,25 @@ def main():
             f.write(translated)
         print('ファイル出力が完了しました。')
 
-    except:
-        print('Error occured.')
+    except AttributeError as e:
+        print(e)
         d.close()
+        print('失敗しました。')
+
+    except exceptions.ElementClickInterceptedException as e:
+        print(e)
+        d.close()
+        print('失敗しました。')
 
 
 if __name__ == '__main__':
     main()
+
+    # Windows, WSLで実行された場合に限り、出力結果をメモ帳で開く。
+    if os.name in ('nt', 'posix'):
+        print('メモ帳で開きます。')
+        file_path = (ABS_DIRNAME + r'/mirai_output.txt').replace('/mnt/c/', 'C:/')
+        subprocess.Popen([r'notepad.exe', file_path])
+        print('終了します。')
+        sys.exit()
     input('Press enter to quit.')
